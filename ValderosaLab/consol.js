57fbafc366fc4b1b2389f20b5bcf06d663b6382b
HellaFayn <159890819+HellaFayn@users.noreply.github.com>
2024-05-14 20:06:03 +0800

    Add files via upload

---
+++ ValderosaLab/consol.js
@@ -0,0 +1,13 @@
+for (let i = 40; i >= 10; i--) {
+    if (i % 3 === 0) {
+      console.log(i);
+    }
+  }
+  
+  for (let i = 0; i <= 40; i += 2) {
+    console.log(i);
+  }
+  
+  for (let i = 0; i <= 30; i++) {
+    console.log(i);
+  }
